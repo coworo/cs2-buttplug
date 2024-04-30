bc33aa6d63ed57ccbf201795fc8a909f40657981
coworo <98850730+coworo@users.noreply.github.com>
2024-05-01 02:13:01 +0300

    increased heartbeat

--- src/csgo.rs
+++ src/csgo.rs
@@ -9,7 +9,7 @@ pub fn build_server(port: u16, game_path: PathBuf) -> GSIServer {
         .subscribe_multiple(Subscription::UNRESTRICTED)
         .throttle(Duration::from_millis(50))
         .buffer(Duration::from_millis(50))
-        .heartbeat(Duration::from_millis(5000))
+        .heartbeat(Duration::from_millis(100)) //increased heartbeat for bomb events, theres probably a better way to do those though
         .build();
 
     let mut server = GSIServer::new(config, port);
